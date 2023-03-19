95f809131849330497a2f43c82d5f01be247ad0d
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-03-19 23:44:44 +0700

    allow

--- app/config/allowedOrigins.js
+++ app/config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     'http://139.180.146.179:1001',
+    'http://139.180.146.179:4000',
+    'http://139.180.146.179:3000',
     'http://localhost:3000',
     'http://localhost:4000',
 ];
